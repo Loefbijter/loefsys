f7aa7302f0ce477c235defa1481f41c468e35e20
Mark Boute <bmark0702@gmail.com>
2025-06-26 17:27:34 +0200

    fix rendering on mobile devices

--- loefsys/components/wavy_seperator_bottom.py
+++ loefsys/components/wavy_seperator_bottom.py
@@ -7,7 +7,7 @@ class WavySeperatorTop(Component):
 
     # language=HTML
     template: types.django_html = """
-        <div class="flex flex-col items-center from-secondary to-transparent bg-linear-to-t to-5%">
+        <div class="flex flex-col items-center from-secondary to-transparent bg-linear-to-t to-5% w-screen overflow-x-hidden">
             <svg class="rotate-180 h-[3vh] min-w-[calc(100vw+2px)] w-[100vh]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
                 <path class="wave-shape-fill" opacity="0.33" d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z"></path>
                 <path class="wave-shape-fill" opacity="0.66" d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z"></path>
