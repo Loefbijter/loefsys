68aa9c9fe000fcd7242dbfd4bc7a9eb4c2e9603b
JortRoelofs <41192305+JortRoelofs@users.noreply.github.com>
2025-11-24 20:29:59 +0100

    remove accountinfopage module

--- loefsys/accountinfopage/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Django app for account info page.
-
-In this app, the account info page is defined.
-"""
--- loefsys/accountinfopage/apps.py
+++
@@ -1,9 +0,0 @@
-"""Module containing the configuration for the accountinfopage app."""
-
-from django.apps import AppConfig
-
-
-class AccountinfopageConfig(AppConfig):
-    """The app configuration for the accountinfopage."""
-
-    name = "loefsys.accountinfopage"
